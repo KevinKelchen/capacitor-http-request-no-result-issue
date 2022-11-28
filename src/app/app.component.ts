ad727f6a7903698ee0479ab0a819fc822984d38a
Kevin Kelchen <kelchen@cartegraph.com>
2022-11-28 09:11:20 -0600

    Move repro to clickable buttons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from "@angular/common/http";
 import { Component } from '@angular/core';
-import { firstValueFrom } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,17 +6,4 @@ import { firstValueFrom } from "rxjs";
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private httpClient: HttpClient) {}
-
-  async ngOnInit() {
-    // URL with an encoded space (via the `+` character) works as expected.
-    // const url = 'https://swapi.dev/api/people/?search=r2+d2';
-
-    // URL with a non-encoded space fails on hybrid iOS.
-    const url = 'https://swapi.dev/api/people/?search=r2 d2';
-
-    const result = await firstValueFrom(this.httpClient.get(url));
-
-    console.dir(result);
-  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from '@angular/core';
+import { firstValueFrom } from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    private httpClient: HttpClient
+  ) {}
 
-  constructor() {}
+  async requestWithEncodedSpace() {
+    // URL with an encoded space (via the `+` character) works as expected.
+    const url = 'https://swapi.dev/api/people/?search=r2+d2';
 
+    await this.makeRequest(url);
+  }
+
+  async requestWithoutEncodedSpace() {
+    // URL with a non-encoded space fails on hybrid iOS.
+    const url = 'https://swapi.dev/api/people/?search=r2 d2';
+
+    await this.makeRequest(url);
+  }
+
+  private async makeRequest(url: string) {
+    const result = await firstValueFrom(this.httpClient.get(url));
+
+    console.dir(result);
+  }
 }
